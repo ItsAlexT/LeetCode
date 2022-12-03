af0a7397569122ef4afc57a357b11fa890273ed3
ItsAlexT <alexandru.tof@gmail.com>
2022-12-03 00:57:41 -0500

    Leetcode 977

---
+++ Easy/977-squares-of-a-sorted-array.js
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/squares-of-a-sorted-array/
+
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var sortedSquares = function(nums) {
+    
+    let retArray = nums.map(x => x*x)
+    
+    return retArray.sort((a,b) => a-b)
+};
+
+const nums = [-4,-1,0,3,10]
+console.log(sortedSquares(nums))
+/*
+Example 1:
+    Input: nums = [-4,-1,0,3,10]
+    Output: [0,1,9,16,100]
+    Explanation: After squaring, the array becomes [16,1,0,9,100].
+    After sorting, it becomes [0,1,9,16,100].
+
+Example 2:
+    Input: nums = [-7,-3,2,3,11]
+    Output: [4,9,9,49,121]
+    */
